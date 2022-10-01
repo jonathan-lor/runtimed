016f6f3efdb92de113b61aa002d326ec78ba5300
Jonathan Lor <jonathan.r.lor@gmail.com>
2022-10-01 14:05:11 -0500

    gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App />
+    <App />
+    <App />
   </React.StrictMode>
 );
 
