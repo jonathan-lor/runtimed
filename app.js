1310e0be2c219534ec2922eb251d7d1e4c3b73a4
Chris <69830673+HorseNuggets@users.noreply.github.com>
2022-10-01 12:01:11 -0500

    express server setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!")
+const express = require("express")
+const app = express()
+const port = 6969
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}/.`)
+})
