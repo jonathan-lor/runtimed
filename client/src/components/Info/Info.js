420dd02073c3035b19c58cab592b335c11849d3a
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2022-10-01 20:33:47 -0500

    added rough draft of info page

--- client/src/components/Info/Info.js
+++ client/src/components/Info/Info.js
@@ -1,9 +1,41 @@
+import './info-style.css'
+import BigOgif from '../images/BigO.gif'
 function Home() {
-    return (
-      <div className="App">
-          Info / instructions go here
+  return (
+    <div id="main_container">
+      <div id="header_container">
+        Information
       </div>
-    );
-  }
-  
-  export default Home;
+      <div id="body_container">
+        <div id="info_container">
+          <div id="introduction_container">
+            <div id="introduction_header_container">
+              What is this program about ?
+            </div>
+            This program will calculate the time complesity of the input
+            alogirthm in order to determin the big O asymptotic notation of the alogirthm.
+            <br></br>
+            <br></br>
+            C++ is used inorder to determine the run time of the function and regression testing
+            will be used in order to determine the big O asymptotic notation.
+          </div>
+        </div>
+        <div id="bigO_container">
+          <div id="BigO_gif_container">
+          <img src={BigOgif} id="BigOgif" alt=""/>
+          </div>
+          <div id="BigO_text_container">
+            <div id="BigO_heaerTxt_container">
+              What is Big O asymptotic notation ?
+            </div>
+              Big O asymptotic notation describes how an alogirthm behave toward large or infitive
+              input. In other word, it is a way of showcasing the time complesity of an alogirthm.
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Home;
